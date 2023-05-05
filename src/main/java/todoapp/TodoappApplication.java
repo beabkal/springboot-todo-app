cc569f61c4414725d41541c758aad548393c2b68
beabkal <b3abkal@gmail.com>
2023-05-05 16:54:18 -0500

    initial commit

---
+++ src/main/java/todoapp/TodoappApplication.java
@@ -0,0 +1,18 @@
+package todoapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
