68172d24e770a7ecb5c05aae412099ca04c4d94f
beabkal <b3abkal@gmail.com>
2023-05-08 11:55:51 -0500

    created Role jpa entity

---
+++ src/main/java/todoapp/entity/Role.java
@@ -0,0 +1,21 @@
+package todoapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
